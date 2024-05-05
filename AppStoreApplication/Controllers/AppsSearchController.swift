415ed6e33b91c7cd5872e1324d383d83991f248b
yernar <yernar.baiginzheyev@gmail.com>
2024-05-05 07:01:33 +0500

    search collection view configurations

---
+++ AppStoreApplication/Controllers/AppsSearchController.swift
@@ -0,0 +1,8 @@
+//
+//  AppsSearchController.swift
+//  AppStoreApplication
+//
+//  Created by Yernar Baiginzheyev on 05.05.2024.
+//
+
+import Foundation
---
+++ AppStoreApplication/Views/SearchResultsCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  SearchResultsCollectionViewCell.swift
+//  AppStoreApplication
+//
+//  Created by Yernar Baiginzheyev on 05.05.2024.
+//
+
+import UIKit
+
+class SearchResultsCollectionViewCell: UICollectionViewCell {
+    
+}
