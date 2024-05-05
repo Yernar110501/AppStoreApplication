9b459e73db992c9598fd50776f41c5081889d157
yernar <yernar.baiginzheyev@gmail.com>
2024-05-05 06:33:17 +0500

    base settings of tabBarController

--- AppStoreApplication/BaseTabBarController.swift
+++ AppStoreApplication/BaseTabBarController.swift
@@ -7,4 +7,27 @@
 
 import UIKit
 
-
+class BaseTabBarController: UITabBarController {
+    // MARK: - Lyfecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        viewControllers = [
+            createNavController(viewController: ViewController(), title: "Today", imageName: "doc.text.image"),
+            createNavController(viewController: ViewController(), title: "Apps", imageName: "square.3.layers.3d.top.filled"),
+            createNavController(viewController: ViewController(), title: "Search", imageName: "magnifyingglass"),
+        ]
+        
+    }
+    
+    // MARK: - Helpers
+    
+    fileprivate func createNavController(viewController: UIViewController, title: String, imageName: String) -> UIViewController {
+        let navController = UINavigationController(rootViewController: viewController)
+        navController.navigationBar.prefersLargeTitles = true
+        viewController.navigationItem.title = title
+        viewController.view.backgroundColor = .systemBackground
+        navController.tabBarItem.title = title
+        navController.tabBarItem.image = UIImage(systemName: imageName)
+        return navController
+    }
+}
