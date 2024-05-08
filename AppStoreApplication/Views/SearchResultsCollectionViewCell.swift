af7def7148eb1727174614ff243d5903a07fe37a
yernar <yernar.baiginzheyev@gmail.com>
2024-05-08 23:12:46 +0500

    cells and UIStackView layouts

--- AppStoreApplication/Views/SearchResultsCollectionViewCell.swift
+++ AppStoreApplication/Views/SearchResultsCollectionViewCell.swift
@@ -14,12 +14,30 @@ class SearchResultsCollectionViewCell: UICollectionViewCell {
     let imageView: UIImageView = {
         let iv = UIImageView()
         iv.backgroundColor = .red
+        iv.layer.cornerRadius = 12
+        iv.widthAnchor.constraint(equalToConstant: 64).isActive = true
+        iv.heightAnchor.constraint(equalToConstant: 64).isActive = true
         return iv
     }()
     
     let nameLabel: UILabel = {
         let label = UILabel()
         label.text = "APP NAME"
+        label.backgroundColor = .yellow
+        return label
+    }()
+    
+    let categoryLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Photo &video"
+        label.backgroundColor = .yellow
+        return label
+    }()
+    
+    let ratingsLabel: UILabel = {
+        let label = UILabel()
+        label.text = "9.26M"
+        label.backgroundColor = .yellow
         return label
     }()
     
@@ -28,13 +46,40 @@ class SearchResultsCollectionViewCell: UICollectionViewCell {
         button.setTitleColor(.blue, for: .normal)
         button.titleLabel?.font = .boldSystemFont(ofSize: 14)
         button.setTitle("GET", for: .normal)
+        button.backgroundColor = .gray
+        button.widthAnchor.constraint(equalToConstant: 80).isActive = true
         return button
     }()
     
     // MARK: - Init
     override init(frame: CGRect) {
         super.init(frame: frame)
-        backgroundColor = .green
+        backgroundColor = .systemBackground
+        
+        let labelsStackView = UIStackView(arrangedSubviews: [
+            nameLabel, categoryLabel, ratingsLabel
+        ])
+        labelsStackView.axis = .vertical
+        
+        
+        let stackView = UIStackView(arrangedSubviews: [
+            imageView,
+            labelsStackView,
+            getButton
+        ])
+        stackView.spacing = 12
+        stackView.alignment = .center
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: self.topAnchor),
+            stackView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 8),
+            stackView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -8),
+            stackView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+        ])
+        
+        
     }
     
     required init?(coder: NSCoder) {
