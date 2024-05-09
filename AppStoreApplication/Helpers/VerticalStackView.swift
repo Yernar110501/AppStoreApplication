b80c6065aae919a8d228a2a60a5607ca6e25dcfa
yernar <yernar.baiginzheyev@gmail.com>
2024-05-09 16:42:09 +0500

    added view extensions and stackview class for vertical stackview

---
+++ AppStoreApplication/Helpers/VerticalStackView.swift
@@ -0,0 +1,20 @@
+//
+//  VerticalStackView.swift
+//  AppStoreApplication
+//
+//  Created by Yernar Baiginzheyev on 09.05.2024.
+//
+
+import UIKit
+
+class VerticalStackView: UIStackView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
