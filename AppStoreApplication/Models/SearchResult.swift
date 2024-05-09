4e982e827f088525965568871a38081035528300
yernar <yernar.baiginzheyev@gmail.com>
2024-05-09 20:06:08 +0500

    created network layer

---
+++ AppStoreApplication/Models/SearchResult.swift
@@ -0,0 +1,8 @@
+//
+//  SearchResults.swift
+//  AppStoreApplication
+//
+//  Created by Yernar Baiginzheyev on 09.05.2024.
+//
+
+import Foundation
---
+++ AppStoreApplication/Service/Service.swift
@@ -0,0 +1,8 @@
+//
+//  Service.swift
+//  AppStoreApplication
+//
+//  Created by Yernar Baiginzheyev on 09.05.2024.
+//
+
+import Foundation
